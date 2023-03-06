82334501569fe25e88e762f34915d2312936b29f
Anisha Hirani <anisha.hirani11@gmail.com>
2023-03-06 14:20:54 +0000

    functionality for clear and delete methods

--- script.js
+++ script.js
@@ -5,11 +5,13 @@ class Calculator {
   }
   
   clear() {
-
+    this.currentOperand = ''
+    this.previousOperand = ''
+    this.operation = undefined
   }
 
   delete() {
-
+    this.currentOperand = this.currentOperand.toString().slice(0, -1)
   }
 
   appendNumber() {
